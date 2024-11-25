a3902b7b742345516813bd33536c1e6fd0d4a049
HopeStar4453 <jameslee82027@gmail.com>
2024-11-25 02:28:26 +0000

    fix:model

--- src/components/dashboard/HomePage.tsx
+++ src/components/dashboard/HomePage.tsx
@@ -1,16 +1,17 @@
-import { twMerge } from 'tailwind-merge'
 import Card from '../tailus-ui/Card'
 import { useEffect } from 'react'
 import { Settings } from 'lucide-react'
 import { ChevronDown } from 'lucide-react'
 import BASE from '/Base.svg'
 import ARBITRUM from '/arbitrum.svg'
-import { useState } from 'react'
+import LOGO from '/logo.jpg'
+import { useState, ChangeEvent } from 'react'
 import Data from '../data'
 import { DynamicWidget, useDynamicContext } from '@dynamic-labs/sdk-react-core'
 import ChainItem from '../utilities/ChainItem'
 import SelectTokenModal from '../utilities/SelectTokenModal'
 import { base, arbitrum } from 'viem/chains'
+// import Background from '../utilities/Background'
 const Icon = [
   { icon: ARBITRUM, name: 'Arbitrum', chainId: arbitrum.id },
   { icon: BASE, name: 'Base', chainId: base.id }
@@ -23,16 +24,19 @@ type selectedTokenType = {
   name: string
   symbol: string
   logoURI: string
+  address: string
 }
 const ARB: selectedTokenType = {
   logoURI: 'https://arbitrum.foundation/logo.png',
   name: 'Arbitrum',
-  symbol: 'ARB'
+  symbol: 'ARB',
+  address: ''
 }
 const USDC: selectedTokenType = {
   logoURI: 'https://ethereum-optimism.github.io/data/USDC/logo.png',
   name: 'USD Coin',
-  symbol: 'USDC'
+  symbol: 'USDC',
+  address: ''
 }
 function Homepage () {
   const [isSelectChain, setSelectChain] = useState(false)
@@ -41,30 +45,61 @@ function Homepage () {
   // const [range, setRange] = useState('Max')
   const range = 'Max'
   const [chain, setChain] = useState(0)
-  const [walletBalance, setWalletBalance] = useState('0')
-  // const UNISWAP_TOKEN_LIST = "https://gateway.ipfs.io/ipns/tokens.uniswap.org";
-  // const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"; //
+  // const [walletBalance, setWalletBalance] = useState('0')
   const [myTokenList, setMyTokenList] = useState<any>(null)
   const [selectedToken, setSelectedToken] = useState<selectedTokenType>(ARB)
   const [selectedTokenBalance, setSelectedTokenBalance] = useState('')
   const [show, setShow] = useState(false)
-
+  const [amount, setAmount] = useState('')
+  const [tokenPrice, setTokenPrice] = useState(0)
   const { primaryWallet } = useDynamicContext()
-  // const config = {
-  //   headers: {
-  //     'Access-Control-Allow-Origin': '*',
-  //     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
-  //   }
-  // }
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false)
+
+  const setSelectedTokenInfo = (item: any) => {
+    setSelectedToken(item)
+    // console.log('token info ', item)
+    // console.log('token amount', selectedTokenBalance)
+
+    const tokenAddress = item.address // Replace with your token address
+    const url = `https://api.dexscreener.com/latest/dex/tokens/${tokenAddress}`
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log('Token Price:', data.pairs[0].priceUsd) // Adjust based on the response structure
+        setTokenPrice(data.pairs[0].priceUsd)
+      })
+      .catch(error => {
+        setTokenPrice(0)
+        console.log('price error', error)
+      })
+  }
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    if (value.length && value[0] != '0' && value[0] != '.') {
+      let inValue: string = value[value.length - 1]
+      if (inValue === '.' || (inValue >= '0' && inValue <= '9')) {
+        setAmount(value)
+      }
+    } else setAmount('')
+  }
+  useEffect(() => {
+    if (amount != '') {
+      if (parseFloat(amount) > parseFloat(selectedTokenBalance))
+        setIsButtonDisabled(false)
+      else setIsButtonDisabled(true)
+    } else {
+      setIsButtonDisabled(false)
+    }
+  }, [amount])
   useEffect(() => {
     setMyTokenList(Data.Tokens)
-    // console.log('myTokenList', myTokenList)
   }, [])
   const updateBalance = async () => {
     if (!primaryWallet) return
     primaryWallet?.getBalance().then(balance => {
       if (balance) {
-        setWalletBalance(balance)
+        // setWalletBalance(balance)
       }
     })
   }
@@ -82,7 +117,7 @@ function Homepage () {
   const handleNetworkSwitch = async () => {
     try {
       if (!primaryWallet) {
-        setWalletBalance('0')
+        // setWalletBalance('0')
         return
       }
       await switchNetwork() // Call the switchNetwork function
@@ -96,12 +131,8 @@ function Homepage () {
   }, [chain, primaryWallet]) // Add all dependencies
 
   return (
-    <div
-      className={twMerge(
-        'relative bg-mainbg  lg:mr-0  lg:rounded-t-[--card-radius] w-full h-[80vdh] mx-auto items-center'
-      )}
-    >
-      {myTokenList ? (
+    <div className='w-full h-dvh'>
+      {myTokenList && (
         <SelectTokenModal
           open={show}
           onClose={() => setShow(false)}
@@ -109,15 +140,20 @@ function Homepage () {
           chain={Icon[chain].chainId}
           BasicTokens={BasicTokens[chain]}
           // selectedToken={selectedToken}
-          setSelectedToken={setSelectedToken}
+          setSelectedToken={setSelectedTokenInfo}
           setSelectedTokenBalance={setSelectedTokenBalance}
           selectedToken={selectedToken}
         />
-      ) : (
-        <div></div>
       )}
-      <div className='bg-mainbg sticky top-0 border-b border-borderbg py-4'>
+      <div className='bg-mainbg sticky top-0 border-b border-borderbg py-4 z-50'>
         <div className='mx-auto flex max-w-full items-center justify-end px-2 gap-2'>
+          <div className=' absolute left-4'>
+            <img
+              src={LOGO}
+              alt='LOGO'
+              className='rounded-full w-12 h-12 border-2 border-white'
+            />
+          </div>
           <div className='relative'>
             <div
               className='bg-darkgrey rounded-md flex flex-row justify-between text-gray-500 p-1 gap-1 px-2 py-1 items-center hover:cursor-pointer'
@@ -153,96 +189,121 @@ function Homepage () {
           <DynamicWidget variant='modal' />
         </div>
       </div>
-      <div className='w-full mx-auto pt-6'>
-        <div className='text-white text-center text-4xl '>
-          Unleash Your Invesment Potential
-        </div>
-        <div className='text-gray-400 text-xl text-center'>
-          Secure, innovative, and high-yield opportunities in the
-        </div>
-        <div className='text-gray-400 text-xl text-center items-center'>
-          decentralized finance landscape
-        </div>
-      </div>
-      <div className='w-full h-4/5 py-6 flex justify-center items-center'>
-        <Card className='max-w-lg bg-cardbg border-borderbg flex flex-col gap-6'>
-          <div className='text-white text-4xl text-center'>
-            Srategic vault Deposit
+      <div
+        className={
+          'relative w-full h-full bg-mainbg  lg:mr-0  lg:rounded-t-[--card-radius] mx-auto'
+        }
+      >
+        <div className='mx-auto pt-6'>
+          <div className='text-white text-center text-4xl '>
+            Unleash Your Invesment Potential
           </div>
-          <div className='flex flex-row justify-center items-center gap-1'>
-            <div className='text-gray-200 text-2xl'>Performance Fee: 1%</div>
-            <div className='text-gray-200 text-2xl  flex flex-row items-center gap-2'>
-              <Settings />
+          <div className='text-gray-400 text-xl text-center'>
+            Secure, innovative, and high-yield opportunities in the
+          </div>
+          <div className='text-gray-400 text-xl text-center items-center'>
+            decentralized finance landscape
+          </div>
+        </div>
+        <div className='w-full py-6 flex justify-center items-center'>
+          <Card className='max-w-lg bg-cardbg border-borderbg flex flex-col gap-6'>
+            <div className='text-white text-4xl text-center'>
+              Srategic vault Deposit
             </div>
-          </div>
-          <div className='bg-modalbg rounded-xl border border-borderbg bg-mediumred flex flex-col gap-2 p-3'>
-            <div className='flex flex-row justify-between items-baseline'>
-              <input
-                className='text-5xl bg-modalbg outline-none text-gray-300 w-full p-0 m-0'
-                placeholder='0'
-              ></input>
-              <div
-                className='bg-darkgrey rounded-md flex flex-row text-gray-500 p-1 gap-1 py-1 items-center hover:cursor-pointer'
-                onClick={() => setShow(true)}
-              >
-                <div className=' relative w-10 h-10 flex flex-row items-end'>
-                  <img
-                    src={
-                      selectedToken?.logoURI
-                        ? selectedToken?.logoURI
-                        : Icon[chain].icon
-                    }
-                    alt='ETH'
-                    className='w-10 h-10 rounded-full'
-                  ></img>
-                  <div className='bg-blue-950 w-5 h-5 absolute bottom  right-0 border-2 rounded-sm border-blue-950'>
-                    <img src={Icon[chain].icon} alt='ETH'></img>
+            <div className='flex flex-row justify-center items-center gap-1'>
+              <div className='text-gray-200 text-2xl'>Performance Fee: 1%</div>
+              <div className='text-gray-200 text-2xl  flex flex-row items-center gap-2'>
+                <Settings />
+              </div>
+            </div>
+            <div className='bg-modalbg rounded-xl border border-borderbg bg-mediumred flex flex-col gap-2 p-3'>
+              <div className='flex flex-row justify-between items-baseline'>
+                <input
+                  className='text-5xl bg-modalbg outline-none text-gray-300 w-full p-0 m-0'
+                  placeholder='0'
+                  value={amount}
+                  onChange={handleInputChange}
+                ></input>
+                <div
+                  className='bg-darkgrey rounded-md flex flex-row text-gray-500 p-1 gap-1 py-1 items-center hover:cursor-pointer'
+                  onClick={() => setShow(true)}
+                >
+                  <div className=' relative w-10 h-10 flex flex-row items-end'>
+                    <img
+                      src={
+                        selectedToken?.logoURI
+                          ? selectedToken?.logoURI
+                          : Icon[chain].icon
+                      }
+                      alt='ETH'
+                      className='w-10 h-10 rounded-full'
+                    ></img>
+                    <div className='bg-blue-950 w-5 h-5 absolute bottom  right-0 border-2 rounded-sm border-blue-950'>
+                      <img src={Icon[chain].icon} alt='ETH'></img>
+                    </div>
                   </div>
-                </div>
 
-                <ChevronDown />
+                  <ChevronDown />
+                </div>
+              </div>
+              <div className='flex flex-row justify-between items-baseline'>
+                <div className='text-gray-400 text-xl'>
+                  {parseFloat(amount) > 0
+                    ? '$' + (parseFloat(amount) * tokenPrice).toFixed(3)
+                    : '-'}
+                </div>
+                <div className='text-gray-500 text-xl'>
+                  {selectedTokenBalance !== ''
+                    ? 'Balance: ' + selectedTokenBalance
+                    : ''}
+                </div>
               </div>
             </div>
-            <div className='flex flex-row justify-between items-baseline'>
-              <div className='text-gray-400 '>-</div>
-              <div className='text-gray-500 text-xl'>
-                Balance:{' '}
-                {selectedTokenBalance !== ''
-                  ? selectedTokenBalance
-                  : Number(walletBalance).toFixed(4)}
+
+            <div className='w-full'>
+              <div className='flex row justify-between items-center gap-1'>
+                {Data.Range.map((item, index) => {
+                  return (
+                    <button
+                      key={index}
+                      className={`w-full bg-buttonbg rounded-xl border  ${
+                        item === range
+                          ? 'border-white text-white font-bold'
+                          : 'border-borderbg'
+                      } hover:border-white py-2 text-gray-500 flex flex-row justify-center gap-2`}
+                      // onClick={() => handleRangeClick(item)}
+                    >
+                      {item}
+                    </button>
+                  )
+                })}
               </div>
             </div>
-          </div>
-          <div className='w-full'>
-            <div className='flex row justify-between items-center gap-1'>
-              {Data.Range.map((item, index) => {
-                return (
-                  <button
-                    key={index}
-                    className={`w-full bg-buttonbg rounded-xl border  ${
-                      item === range
-                        ? 'border-white text-white font-bold'
-                        : 'border-borderbg'
-                    } hover:border-white py-2 text-gray-500 flex flex-row justify-center gap-2`}
-                    // onClick={() => handleRangeClick(item)}
-                  >
-                    {item}
-                  </button>
-                )
-              })}
-            </div>
-          </div>
-          <button className='bg-red-700 p-2 shadow-lg rounded-lg text-white border border-red-500 hover:border-white'>
-            Deposit and Start Earning
-          </button>
-        </Card>
-      </div>
-      <div className='mx-auto w-full'>
-        <div className='text-gray-400 text-lg text-center'>
-          Cerberus Inu: Guarding your assets with cutting-edge DeFi
+            <button
+              className={`${
+                isButtonDisabled
+                  ? 'bg-red-700 hover:border-white text-white  border-red-500'
+                  : 'bg-buttonbg text-gray-300 border-gray-600'
+              } p-2 shadow-lg rounded-lg border `}
+              disabled={isButtonDisabled}
+            >
+              {parseFloat(amount) <= parseFloat(selectedTokenBalance)
+                ? parseFloat(amount) > 0
+                  ? 'Start Approve'
+                  : 'Deposit and Start Earning'
+                : amount != ''
+                ? 'Insufficient Balance'
+                : 'Deposit and Start Earning'}
+            </button>
+          </Card>
         </div>
-        <div className='text-gray-400 text-xl text-center'>
-          strategies and multi-layered security protocols.
+        <div className='mx-auto w-full'>
+          <div className='text-gray-400 text-lg text-center'>
+            Cerberus Inu: Guarding your assets with cutting-edge DeFi
+          </div>
+          <div className='text-gray-400 text-xl text-center'>
+            strategies and multi-layered security protocols.
+          </div>
         </div>
       </div>
     </div>
